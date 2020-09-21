1c4e0934f63d101bece7359f7d10ad471b6091f3
Andrew Deick <37886715+adeick@users.noreply.github.com>
2020-09-21 03:08:08 -0500

    Update dbInit.js

--- ig88/dbInit.js
+++ ig88/dbInit.js
@@ -1,11 +1,11 @@
-const Sequelize = require('sequelize');
+// const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('database', 'username', 'password', {
-	host: 'localhost',
-	dialect: 'sqlite',
-	logging: false,
-	storage: 'database.sqlite',
-});
+// const sequelize = new Sequelize('database', 'username', 'password', {
+// 	host: 'localhost',
+// 	dialect: 'sqlite',
+// 	logging: false,
+// 	storage: 'database.sqlite',
+// });
 
-const Users = require('./models/Users')(sequelize, Sequelize.DataTypes);
-sequelize.sync();
+// const Users = require('./models/Users')(sequelize, Sequelize.DataTypes);
+// sequelize.sync();
