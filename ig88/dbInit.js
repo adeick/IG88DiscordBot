9440ae23ac4d6537eb079bd3d6aa6b07a4ed75ed
Andrew Deick <37886715+adeick@users.noreply.github.com>
2020-09-22 01:12:14 -0500

    Test Slogans

--- ig88/dbInit.js
+++ ig88/dbInit.js
@@ -1,11 +1,15 @@
-// const Sequelize = require('sequelize');
+const Sequelize = require('sequelize');
 
-// const sequelize = new Sequelize('database', 'username', 'password', {
-// 	host: 'localhost',
-// 	dialect: 'sqlite',
-// 	logging: false,
-// 	storage: 'database.sqlite',
-// });
+const sequelize = new Sequelize('database', 'username', 'password', {
+	host: 'localhost',
+	dialect: 'sqlite',
+	logging: false,
+	storage: 'database.sqlite',
+});
 
-// const Users = require('./models/Users')(sequelize, Sequelize.DataTypes);
-// sequelize.sync();
+require('./models/Users')(sequelize, Sequelize.DataTypes);
+
+sequelize.sync({force}).then(async() => {
+    console.log('Database synced');
+    sequelize.close();
+}).catch(console.error);
--- ig88/models/Users.js
+++ ig88/models/Users.js
@@ -1,15 +1,15 @@
-// module.exports = (sequelize, DataTypes) => {
-// 	return sequelize.define('users', {
-// 		user_id: {
-// 			type: DataTypes.STRING,
-// 			primaryKey: true,
-// 		},
-// 		slogan: {
-// 			type: DataTypes.STRING,
-// 			defaultValue: "",
-// 			allowNull: true,
-// 		}
-// 	}, {
-// 		timestamps: false,
-// 	});
-// };
+module.exports = (sequelize, DataTypes) => {
+	return sequelize.define('users', {
+		user_id: {
+			type: DataTypes.STRING,
+			primaryKey: true,
+		},
+		slogan: {
+			type: DataTypes.STRING,
+			defaultValue: "",
+			allowNull: true,
+		}
+	}, {
+		timestamps: false,
+	});
+};
