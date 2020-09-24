8999c0e6516f8b4f61ae54ede4d4f0b483216c46
Andrew Deick <37886715+adeick@users.noreply.github.com>
2020-09-24 00:51:44 -0500

    Initialize Database differently!

--- ig88/dbInit.js
+++ ig88/dbInit.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('database', 'username', 'password', {
-	host: 'localhost',
+const sequelize = new Sequelize('database', 'root', 'T9s+Y(xocjT}#NCS', {
+	host: '144.202.21.9',
 	dialect: 'sqlite',
 	logging: false,
 	storage: 'database.sqlite',
