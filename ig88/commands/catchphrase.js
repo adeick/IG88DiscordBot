4435f208180feca33c69f7204548ffd83b86480c
Andrew Deick <37886715+adeick@users.noreply.github.com>
2020-09-24 00:19:19 -0500

    Relative Filepaths

--- ig88/commands/catchphrase.js
+++ ig88/commands/catchphrase.js
@@ -1,4 +1,4 @@
-const Users = require("*/models/Users");
+const Users = require("/ig88/models/Users");
 const Discord = require('discord.js');
 
 //const catchphrase = require('../catchphrase.sqlite');
