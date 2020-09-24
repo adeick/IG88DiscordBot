ccfba106f529ad32a42454d1c702c65597e5b6d5
Andrew Deick <37886715+adeick@users.noreply.github.com>
2020-09-24 00:16:41 -0500

    Finding database

--- ig88/commands/catchphrase.js
+++ ig88/commands/catchphrase.js
@@ -1,4 +1,4 @@
-const Users = require("../models/Users");
+const Users = require("*/models/Users");
 const Discord = require('discord.js');
 
 //const catchphrase = require('../catchphrase.sqlite');
