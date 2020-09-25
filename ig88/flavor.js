72394af366ed84431f4ac22c08ce58cd22183da6
Andrew Deick <37886715+adeick@users.noreply.github.com>
2020-09-25 02:10:56 -0500

    Update flavor.js

--- ig88/flavor.js
+++ ig88/flavor.js
@@ -12,15 +12,15 @@ exports.execute = (message) => {
         }
     }
 
-    if(args.includes("fuck") && message.channel.name != "shenanigans" && message.author.tag != "Darth_Vader#4942"){
-        let random = Math.floor(Math.random() * 6); //0 - 5
-        if(random == 0){
-            message.channel.send(`Fucking hell! Who is teaching you these naughty words?`);
-        }
-        if(random == 1){
-            message.channel.send(`May I be so inclined to ask, whatever happened to polite conversation?`);
-        }
-    }
+    // if(args.includes("fuck") && message.channel.name != "shenanigans" && message.author.tag != "Darth_Vader#4942"){
+    //     let random = Math.floor(Math.random() * 6); //0 - 5
+    //     if(random == 0){
+    //         message.channel.send(`Fucking hell! Who is teaching you these naughty words?`);
+    //     }
+    //     if(random == 1){
+    //         message.channel.send(`May I be so inclined to ask, whatever happened to polite conversation?`);
+    //     }
+    // }
 
     if(message.author.tag == "ricochetzghost#3463" && message.channel.name == "shenanigans"){
         let random = Math.floor(Math.random() * 100); //0 - 99
