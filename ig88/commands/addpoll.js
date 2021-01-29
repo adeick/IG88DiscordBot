3ddc79854e7ee28d0d6d557976b8f47d15fda030
Andrew Deick <37886715+adeick@users.noreply.github.com>
2021-01-28 23:56:55 -0600

    Create addpoll.js

---
+++ ig88/commands/addpoll.js
@@ -0,0 +1,26 @@
+module.exports = {
+  name: 'addpoll',
+  description: 'Gives a person a role',
+  args: true,
+  allGuilds: true,
+  aliases: ["poll", "survey", "react"],
+  guildOnly: true,
+
+	execute(message, args) {
+        let str = "";
+        let i;
+        for(i = 0; i < args.length; i++){
+            if(args[i].match(/(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/g)){
+                break;
+            }
+            else{
+                str += " " + args[i];
+            }
+        }
+//        if(message.author.id == '315672211518128128'){
+        message.channel.send(str);	
+        for(i = i; i < args.length; i++){
+            message.react(args[i]);
+        }
+  },
+};
