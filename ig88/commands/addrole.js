fd1b156727fa139d568051594806e6ec4c3e648e
Andrew Deick <37886715+adeick@users.noreply.github.com>
2021-09-20 11:53:01 -0500

    Update addrole.js

--- ig88/commands/addrole.js
+++ ig88/commands/addrole.js
@@ -10,7 +10,7 @@ module.exports = {
 	execute(message, args) {
         let member = message.mentions.members.first(); //Finds the first mentioned user
         
-        if(!member || member.guild.name === "Easy Coding"){
+        if(member && member.guild.name === "Easy Coding"){
             message.channel.send(`Found ${member}`);
             member.roles.add('734679694590476338'); //Awesome
             message.channel.send("Added an awesome role!");
