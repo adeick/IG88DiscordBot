cd030e7c2e94045f5f88ee94b70300c4b38323e8
Andrew Deick <37886715+adeick@users.noreply.github.com>
2021-10-15 00:09:29 -0500

    Update talk.js

--- ig88/commands/talk.js
+++ ig88/commands/talk.js
@@ -35,7 +35,7 @@ module.exports = {
         let str = interaction.options.getString('speech', true);
         if(str){
             interaction.channel.send(str);
-            await interaction.reply({ content: ""});
+            await interaction.reply({ content: "..."});
             return interaction.deleteReply();
         }
         return interaction.reply({ content: "You need to give me something to say!", ephemeral: true });
